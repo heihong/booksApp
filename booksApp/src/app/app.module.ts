3e54dde5946451fc286b7fac47be57559b1fcd7a
hei-hongtraing <traingheihong@gmail.com>
2019-09-27 13:30:03 +0200

    move books component

--- booksApp/src/app/app.module.ts
+++ booksApp/src/app/app.module.ts
@@ -4,19 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CartModule} from './cart/cart.module';
-import {BooksModule} from './containers/books/books.module';
+import {BooksComponent} from './containers/books/books.component';
+import {FilterPipe} from './services/pipe/filter.pipe';
+import {BookComponent} from './components/book/book.component';
+import {CartData} from './cartData/cartData';
+import {BooksService} from './services/books/books.service';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryBooksService} from './memoryData/InMemoryBooksService';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent ,
+    FilterPipe,
+    BookComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CartModule,
-    BooksModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryBooksService, { dataEncapsulation: false },
+    )
   ],
-  providers: [],
+  providers: [CartData, BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
